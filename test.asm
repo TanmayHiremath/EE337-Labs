136bea7ad719de8f0ebb384bf84747b28474b3fd
TanmayHiremath <tanmay.hiremath@gmail.com>
2021-02-13 16:09:02 +0530

    lab 4 done

--- test.asm
+++ test.asm
@@ -1,22 +1,5 @@
-org 0000h
-ljmp start
-org 100h
-start:
-mov r0,#8
-mov a,#0D6H
-mov b,#96H
-mul AB
-rep:
-rlc a
-jnc next
-clr c
-next:
-djnz r0,rep
-jz insert
-sjmp here
-insert:
-add a,#0ACH
-
-here:
-sjmp here
-end
+MOV A,#69H
+ADD A,#48H
+DA A
+RRC A
+DA A
