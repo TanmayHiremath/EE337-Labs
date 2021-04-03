55e9f16536bf82c1232a1ae6b9fa043870eee8c2
Tanmay Hiremath <tanmay.hiremath@gmail.com>
2021-04-03 11:47:26 +0530

    project started

--- Project/functions.h
+++ Project/functions.h
@@ -1,5 +1,9 @@
+#include <at89c5131.h>
+#include "lcd.h"	  //Header file with LCD interfacing functions
+#include "serial.c" //C file with UART interfacing functions
+#include <string.h>
 #include <stdlib.h>
-
+int consta[4] = {2000, 500, 200, 100};
 // char *doubleDigitString(int x)
 // {
 // 	char str[80];
@@ -14,16 +18,50 @@
 // 		return str;
 // 	}
 // }
-void numberNotes(char *x,int array[])
+// void numberNotes(char *x,int array[])
+// {
+// 	int n = atoi(x);
+// 	int arr[4];
+// 	int consta[4] = {2000, 500, 200, 100};
+// 	int i;
+// 	for (i = 0; i < 4; i++)
+// 	{
+// 		arr[i] = n / consta[i];
+// 		n = n % consta[i];
+// 	}
+
+// }
+void invalidInput()
+{
+	transmit_string("Invalid input\r\n");
+	lcd_cmd(0x01);
+	lcd_cmd(0xC0);
+	lcd_write_string("Invalid input");
+	msdelay(2000);
+	lcd_cmd(0x01);
+}
+int numberNotes(char *x, int number[])
 {
 	int n = atoi(x);
 	int arr[4];
-	int consta[4] = {2000, 500, 200, 100};
 	int i;
 	for (i = 0; i < 4; i++)
 	{
 		arr[i] = n / consta[i];
-		n = n % consta[i];
+		if (arr[i] > number[i])
+		{
+			n -= number[i] * consta[i];
+			number[i] = 0;
+		}
+		// n = n % consta[i];
+		else
+		{
+			n -= arr[i] * consta[i];
+			number[i] -= arr[i];
+		}
 	}
-
+	if (n == 0)
+		return 1;
+	else
+		return 0;
 }
--- Project/main.c
+++ Project/main.c
@@ -1,39 +1,12 @@
-#include <at89c5131.h>
-#include "lcd.h"	  //Header file with LCD interfacing functions
-#include "serial.c" //C file with UART interfacing functions
-#include <string.h>
-#include <stdlib.h>
-// #include "functions.h"
+
+#include "functions.h"
 sbit LED = P1 ^ 7;
-
-void invalidInput()
-{
-	transmit_string("Invalid input\r\n");
-	lcd_cmd(0x01);
-	lcd_cmd(0xC0);
-	lcd_write_string("Invalid input");
-	msdelay(2000);
-	lcd_cmd(0x01);
-}
-void numberNotes(char *x, int array[])
-{
-	int n = atoi(x);
-	int arr[4];
-
-	int i;
-	for (i = 0; i < 4; i++)
-	{
-		arr[i] = n / consta[i];
-		n = n % consta[i];
-	}
-	
-}
+int number[4] = {20, 30, 10, 0};
 
 //Main function
 void main(void)
 {
-	int number[4] = {20, 30, 10, 10};
-	int consta[4] = {2000, 500, 200, 100};
+
 	char str1[10] = "";
 	unsigned char ch = 0;
 	//Initialize port P1 for output from P1.7-P1.4
@@ -59,7 +32,11 @@ void main(void)
 			}
 			else
 			{
-				numberNotes(str1, number);
+
+				if (numberNotes(str1, number))
+					lcd_write_string("Collect Cash");
+				else
+					lcd_write_string("Try Again");
 			}
 			break;
 		case '0':
